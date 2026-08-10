4b9d5971585dd772f6497c89f69105750cdfcd22
AlmeidaGallonte <jamersonalmeida4567@gmail.com>
2026-08-10 15:39:43 -0300

    aula 11 completa

--- aulas/#11-Estruturas_repetição3.py
+++ aulas/#11-Estruturas_repetição3.py
@@ -13,7 +13,7 @@ for par in range(valor,0,-2):
     print(par)'''
 
 #Quantos valore de 0 a 10
-quais_valores = []
+'''quais_valores = []
 entr_0e10 = 0
 soma_impar = 0
 for c in range(0,6):
@@ -23,4 +23,11 @@ for c in range(0,6):
         quais_valores.append(numero)
         if numero%2 == 1:
             soma_impar += numero
-print(f'No total foram {entr_0e10} números entre 0 e 10 que foram {quais_valores} e a soma dos números impares digitados é {soma_impar}')
+print(f'No total foram {entr_0e10} números entre 0 e 10 que foram {quais_valores} e a soma dos números impares digitados é {soma_impar}')'''
+
+#Combinacões
+'''for c1 in range(1,4):
+    for c2 in range(1,4):
+        print(c1, c2)'''
+
+
